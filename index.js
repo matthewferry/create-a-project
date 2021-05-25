e3a944dc1544bcbac52efb992fb30b9c12461589
Matthew Ferry <matthewferry@github.com>
2021-05-25 23:13:41 +0000

    Try replace for columns

--- index.js
+++ index.js
@@ -16,26 +16,10 @@ async function run() {
       repo: context.repo.repo,
       name: core.getInput('name'),
     });
-
-    const parseColumns = (c) => {
-      c.split(/\r?\n/).reduce((acc, line) => {
-        acc
-          .concat(line.split(","))
-          .filter(pat => pat)
-          .map(pat => pat.trim())
-      }, []);
-    };
     
-    // columns.split(/\r?\n/);
+    columns.replace(/\r?\n/g, ", ");
 
-    console.log(parseColumns(columns));
-
-    parseColumns(columns).forEach((column) => {
-      octokit.rest.projects.createColumn({
-        project_id: createProject.data.id,
-        name: column,
-      });
-    });
+    console.log(columns);
 
     core.setOutput('project-id', createProject.data.id);
   } catch (error) {
